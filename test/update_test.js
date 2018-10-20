eebd7b86ef36bcfa68a7728efcc2c5dfec2e42fd
Greg Borrelly <gregborrelly@gmail.com>
2018-10-20 00:33:50 -0400

    virtual functions

--- test/update_test.js
+++ test/update_test.js
@@ -4,7 +4,7 @@ const User  = require('../users/src/user')
 describe('Updating a user', () => {
     let joe
     beforeEach((done)=>{
-        joe = new User({ name: 'Joe', postCount:0})
+        joe = new User({ name: 'Joe', likes:0})
         joe.save()
             .then(()=>{
                 done()
@@ -44,11 +44,12 @@ describe('Updating a user', () => {
     it('A model class can update', (done) => {
         assertName(User.findByIdAndUpdate(joe._id, { name: 'Alex'}), done)
     })
-    xit('Increment user postCout by 1', (done) => {
-        User.update({ name: 'Joe'}, { $inc: { postCount: 1}})
+    
+    it('Increment user postCout by 1', (done) => {
+        User.update({ name: 'Joe'}, { $inc: { likes: 1}})
             .then( () => User.findOne({ name: 'Joe'}))
             .then( (user) => {
-                assert(user.postCount === 1)
+                assert(user.likes === 1)
                 done()
             })
     })
